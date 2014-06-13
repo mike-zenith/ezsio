8ff234eeb1bbe5ae89902bc077ce99c04ac27ddc
Imre Juhasz <awe.zenith@gmail.com>
2014-06-13 10:50:35 +0200

    added npm install to start

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(80);
+app.listen(80, function () {
+    console.log('listening...');
+});
 
 function handler (req, res) {
     fs.readFile(__dirname + '/index.html',
