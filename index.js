c58f041e706cea7c59ef398e4cca75e6df97d0c0
Imre Juhasz <awe.zenith@gmail.com>
2014-06-13 09:26:44 +0200

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.on('connection', function (socket) {
+    console.log('hello ->', socket.id);
+    socket.emit('hello', { hello: socket.id });
+
+    socket.on('echo', function (data) {
+        console.log('-> echo', data);
+        socket.emit('echo', data);
+    });
+
+    socket.on('message', function (data) {
+        console.log('-> message', data);
+    });
+});
